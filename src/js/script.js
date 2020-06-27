74bc40b177228bce75a1798600342940b3f9e816
Sashko <sashkoace@gmail.com>
2020-06-27 21:04:35 +0300

    apdated main page

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,42 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    window.addEventListener('DOMContentLoaded', () => {
-        const menu = document.querySelector('.marathon-nav__wrapper'),
-            menuItem = document.querySelectorAll('.marathon-nav__item'),
-            hamburger = document.querySelector('.hamburger');
+    // __________HAMBURGER__________
+    // window.addEventListener('DOMContentLoaded', () => {
+    //     const menu = document.querySelector('.marathon-nav__wrapper'),
+    //         menuItem = document.querySelectorAll('.marathon-nav__item'),
+    //         hamburger = document.querySelector('.hamburger');
 
-        hamburger.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('marathon-nav__wrapper_active');
+    //     hamburger.addEventListener('click', () => {
+    //         hamburger.classList.toggle('hamburger_active');
+    //         menu.classList.toggle('marathon-nav__wrapper_active');
+    //     });
+
+    //     menuItem.forEach(item => {
+    //         item.addEventListener('click', () => {
+    //             hamburger.classList.toggle('hamburger_active');
+    //             menu.classList.toggle('marathon-nav__wrapper_active');
+    //         })
+    //     })
+    // })
+
+
+    // __________TYPED.JS__________
+    const typed = new Typed('#typed', {
+        strings: [
+            'Почни вдосконалювати<br>свій голос просто зараз!',
+            'Давай зробимо<br>з нього цукерочку!',
+        ],
+        typeSpeed: 80,
+        backSpeed: 30,
+        smartBackspace: true,
+        loop: true
+    });
+
+    // __________SLICK__________
+    $(document).ready(function(){
+        $('.slick').slick({
+            adaptiveHeight: true,
+            // autoplay: true,
+            dots: true,
         });
-
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('marathon-nav__wrapper_active');
-            })
-        })
-    })
+    });
 });

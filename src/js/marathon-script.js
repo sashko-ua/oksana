d516c0442e77a260b96e944a36e7fa97967d60e8
Sashko <sashkoace@gmail.com>
2020-07-08 18:03:34 +0300

    bugs fixed

--- src/js/marathon-script.js
+++ src/js/marathon-script.js
@@ -1,20 +1,20 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     // __________HAMBURGER__________
     const menu = document.querySelector('.marathon-nav__wrapper'),
         menuItem = document.querySelectorAll('.marathon-nav__item'),
         hamburger = document.querySelector('.hamburger');
 
-        hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('marathon-nav__wrapper_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
             menu.classList.toggle('marathon-nav__wrapper_active');
-        });
-
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('marathon-nav__wrapper_active');
-            })
+        })
     })
 
     // __________SLICK__________
@@ -22,4 +22,20 @@ $(document).ready(function(){
         adaptiveHeight: true,
         autoplay: true,
     });
+
+    // __________SMOOTH SCROLLING__________
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute("href").substr(1);
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+        });
+    }
 });
